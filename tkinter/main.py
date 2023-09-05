66ae32d842783b3e1e088333560d0341904efc22
Alexander Garcia <31865557+Alexander-Garcia@users.noreply.github.com>
2023-09-05 11:40:53 +0200

    feat: tkinter layouts

--- tkinter/main.py
+++ tkinter/main.py
@@ -3,17 +3,27 @@ import tkinter
 window = tkinter.Tk()
 window.title("Gui test")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 
 # Label
 test_label = tkinter.Label(text="Blah blah derp", font=("Arial", 24, "bold"))
-test_label.pack()
+test_label.grid(row=0, column=0)
 
 def button_click():
-    test_label["text"] = "button clicked"
+    input_text = input.get()
+    test_label.config(text=input_text)
 
 # Button
 button = tkinter.Button(text="Click me", command=button_click)
-button.pack()
+button.grid(row=1, column=1)
+
+button_2 = tkinter.Button(text="blah")
+button_2.grid(row=0, column=2)
+
+# Input (Entry)
+input = tkinter.Entry()
+input.grid(row=2, column=3)
+
 
 window.mainloop()
