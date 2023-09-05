51f3f6bd92822d1b504ff6fa914f3887e3afce70
Alexander Garcia <31865557+Alexander-Garcia@users.noreply.github.com>
2023-09-05 19:30:31 +0200

    feat: more api work with internation space station location api

--- apiInto/main.py
+++ apiInto/main.py
@@ -1,10 +1,41 @@
+# The final piece of this is to send myself an email but I'm not putting credentials in here so yeah. 
+# TODO: maybe see how python handles env type variables and put email stuff in there?
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+# constants
+# taken while in Vienna, Austria
+LAT = 48.208176
+LONG = 16.373819
 
-data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-print(longitude)
-print(latitude)
+
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+    "formatted": 0,
+}
+
+
+def get_iss_position():
+    r = requests.get("http://api.open-notify.org/iss-now.json")
+    r.raise_for_status()
+    data = r.json()
+    return {
+        "lat": float(data["iss_position"]["latitude"]),
+        "long": float(data["iss_position"]["longitude"])
+    }
+
+def get_sunset_sunrise():
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise_hour = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset_hour = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    return {
+        "sunrise_hour": sunrise_hour,
+        "sunset_hour": sunset_hour,
+    }
+
+local_hours = get_sunset_sunrise()
+iss_position = get_iss_position()
+current_hour = datetime.now().hour
