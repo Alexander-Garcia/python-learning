27dea638257cc4773401b02d71ae28c795a09cfc
Alexander Garcia <31865557+Alexander-Garcia@users.noreply.github.com>
2023-09-06 22:06:36 +0200

    feat: added error handling to nato phonetic

--- natoAlphabet/main.py
+++ natoAlphabet/main.py
@@ -11,7 +11,14 @@ nato_file = pandas.read_csv(filepath_or_buffer=CSV_FILENAME)
 # iterate through df and create dict
 nato_dict = {row.letter: row.code for (_, row) in nato_file.iterrows()}
 
-user_word = input("Enter a word to receive its phonetic code:\n ").upper()
+def generate_phonetic():
+    user_word = input("Enter a word to receive its phonetic code:\n ").upper()
+    try:
+        phonetic_list = [nato_dict[letter] for letter in user_word]
+    except KeyError:
+        print("only use letters in alphabet")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
 
-phonetic_list = [nato_dict[letter] for letter in user_word]
-print(phonetic_list)
+generate_phonetic()
