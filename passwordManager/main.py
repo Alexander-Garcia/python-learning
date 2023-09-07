13df5f063a3ec84c22b0a866aa52237e5927465c
Alexander Garcia <31865557+Alexander-Garcia@users.noreply.github.com>
2023-09-07 14:07:44 +0200

    feat: added search for website if email password exist already

--- passwordManager/main.py
+++ passwordManager/main.py
@@ -57,6 +57,22 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH WEBSITES ------------------------------- #
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="File not found", message="Unable to find password file")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Not found", message=f"No email and password found for {website}")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -77,8 +93,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -91,5 +107,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=13, command=search)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
